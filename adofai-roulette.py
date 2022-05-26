c2c2c445af97c2758c86f2f49489e8ebd16c9b71
huantian <davidtianli@gmail.com>
2022-05-25 19:04:00 -0700

    light code cleanup

--- adofai-roulette.py
+++ adofai-roulette.py
@@ -2,74 +2,50 @@ import random
 
 # setup song, difficulty, and creator lists
 
-tempfile = open("songs.txt", encoding="utf-8")
-songlistbad = tempfile.readlines()
-tempfile2 = open("difficulties.txt", encoding="utf-8")
-difficultylistbad = tempfile2.readlines()
-tempfile3 = open("creators.txt", encoding = "utf-8")
-creatorlistbad = tempfile3.readlines()
+def readLinesAndStrip(filename: str) -> list[str]:
+    with open(filename, encoding="utf-8") as file:
+        lines = file.readlines()
 
-songlist = []
-difficultylist = []
-creatorlist = []
+    return [line.strip() for line in lines]
 
-for a in songlistbad:
-    songlist.append(a.strip())
-for a in difficultylistbad:
-    difficultylist.append(a.strip())
-for a in creatorlistbad:
-    creatorlist.append(a.strip())
+
+songlist = readLinesAndStrip("songs.txt")
+difficultylist = readLinesAndStrip("difficulties.txt")
+creatorlist = readLinesAndStrip("creators.txt")
 
 # variable stuff
 
 print("type 'balls' to give up, type your percentage to continue")
 
-response = 0
-oldpercent = 0
+percent = 0
 score = 0
 
-#initial loop
+while percent < 100:
+    rng = random.randrange(len(songlist))
+    print(f"{songlist[rng]} - {creatorlist[rng]} - {difficultylist[rng]} - Goal: {percent + 1}%")
+    print()
 
-rng = random.randint(1, len(songlist))
-del songlist[rng]
-print(songlist[rng] + " - " + creatorlist[rng] + " - " + difficultylist[rng] + " - Goal: 1%")
-print("")
-while True:
-    response = input("")
-    try:
-        int(response)
-    except ValueError:
-        continue
-    break
-response = int(response)
-oldpercent = response
-print("")
-
-#real loop
-
-while response < 100:
-    rng = random.randint(1, len(songlist))
-    print(songlist[rng] + " - " + creatorlist[rng] + " - " + difficultylist[rng] + " - Goal: " + str(response + 1) + "%")
-    print("")
     while True:
         response = input("")
+
         if response == "balls":
             exit()
         try:
-            int(response)
+            parsed_response = int(response)
         except ValueError:
             continue
-        if int(response) <= (oldpercent):
+        if parsed_response <= percent:
             continue
-        response = int(response)
+
+        percent = parsed_response
         break
-    print("")
-    oldpercent = response
+
+    print()
     score += 1
     del songlist[rng]
 
 print("s+")
 print("good job!")
-print("score: " + str(score) + " / 100")
+print(f"score: {score} / 100")
 
-input()
+input("Press enter to continue...")
