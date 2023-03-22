aaefa9b112490b8da4ba9e372227997f916594ac
WillTRM <92384657+WillTRM@users.noreply.github.com>
2023-03-22 17:31:43 -0500

    Add files via upload

--- adofai-roulette-gui.py
+++ adofai-roulette-gui.py
@@ -1,6 +1,9 @@
+import json
 import random
 import time as t
+import numpy as np
 import PySimpleGUI as sg
+from tkinter import filedialog
 
 # set up song, difficulty, and creator lists
 
@@ -9,6 +12,23 @@ def readLinesAndStrip(filename: str) -> list[str]:
         lines = file.readlines()
 
     return [line.strip() for line in lines]
+
+#what the fuck does this do
+#i stole it off stack overflow
+#HEY IT WORKS!
+
+class npEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.int32):
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
+
+def openFile():
+    filepath = filedialog.askopenfilename()
+    file = open(filepath, "r")
+    temp = json.load(file)
+    print(temp["levelList"])
+    return temp["levelList"], temp["score"], temp["progress"]
 
 songs = readLinesAndStrip("songs.txt")
 difficulties = readLinesAndStrip("difficulties.txt")
@@ -19,24 +39,22 @@ artists = readLinesAndStrip("artists.txt")
 
 percent = 0
 score = 0
-rng = random.randrange(len(songs))
+rng = list(np.random.permutation(np.arange(len(songs) + 1))[:100])
 
 # set up layout
 
 layout = [
-    [sg.Text(f"Level: {songs[rng]}", key = "levelDisp")],
-    [sg.Text(f"Artist: {artists[rng]}", key = "artistDisp")],
-    [sg.Text(f"Difficulty: {difficulties[rng]}", key = "diffDisp")],
-    [sg.Text(f"Charter: {creators[rng]}", key = "charterDisp")],
+    [sg.Text(f"Level: {songs[rng[percent]]}", key = "levelDisp")],
+    [sg.Text(f"Artist: {artists[rng[percent]]}", key = "artistDisp")],
+    [sg.Text(f"Difficulty: {difficulties[rng[percent]]}", key = "diffDisp")],
+    [sg.Text(f"Charter: {creators[rng[percent]]}", key = "charterDisp")],
     [sg.Text(f"Goal: {percent + 1}%", key = "percentDisp")],
     [sg.Text("Enter percent as number:"), sg.InputText(size = (5, 1))],
-    [sg.Button("Submit", key = "submitButton"), sg.Button("Give Up")]
+    [sg.Button("Submit", key = "submitButton"), sg.Button("Give Up")],
+    [sg.Button("Export", key = "exportSave"), sg.Button("Import", key = "importSave")]
 ]
 
 window = sg.Window("ADOFAI Roulette", layout)
-
-percent = 0
-score = 0
 
 def hide_stuff_i_guess_surely_theres_a_better_way_to_do_this():
     window["artistDisp"].hide_row()
@@ -45,6 +63,7 @@ def hide_stuff_i_guess_surely_theres_a_better_way_to_do_this():
     window["percentDisp"].hide_row()
     window[0].hide_row()
     window["submitButton"].hide_row()
+    
 # code loop
 
 while True:
@@ -53,14 +72,13 @@ while True:
     if event == "submitButton":
         try:
             parsed_response = int(values[0])
+            score += 1
         except ValueError:
-            continue
-        if parsed_response <= percent:
             continue
 
         percent = parsed_response
         if percent == 100:
-            window["levelDisp"].update(f"Congrats! Your score was {score + 1}!")
+            window["levelDisp"].update(f"Congrats! Your score was {score}!")
             hide_stuff_i_guess_surely_theres_a_better_way_to_do_this()
             window.read()
         
@@ -76,14 +94,26 @@ while True:
         
     elif event == sg.WIN_CLOSED:
         break
+
+    elif event == "exportSave":
+        dtw = {
+            "levelList": rng,
+            "progress": percent,
+            "score": score
+        }
+        print(dtw)
+        sd = open("adofai_roulette_save.json", "w+")
+        json.dump(dtw, sd, indent = 4, cls = npEncoder)
+        sd.close()
+
+    elif event == "importSave":
+        rng, score, percent = openFile()
+        print(rng)
     
-    score += 1
-    rng = random.randrange(len(songs))
-    window["levelDisp"].update(f"Level: {songs[rng]}")
-    window["artistDisp"].update(f"Artist: {artists[rng]}")
-    window["diffDisp"].update(f"Difficulty: {difficulties[rng]}")
-    window["charterDisp"].update(f"Charter: {creators[rng]}")
+    window["levelDisp"].update(f"Level: {songs[rng[percent]]}")
+    window["artistDisp"].update(f"Artist: {artists[rng[percent]]}")
+    window["diffDisp"].update(f"Difficulty: {difficulties[rng[percent]]}")
+    window["charterDisp"].update(f"Charter: {creators[rng[percent]]}")
     window["percentDisp"].update(f"Goal: {percent + 1}%")
-    del songs[rng]
         
 window.close()
